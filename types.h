42d9c733f137af6266a86b703b9118bb593eb5b2
mesheets <16882600+mesheets@users.noreply.github.com>
2025-06-11 23:08:48 -0400

    For explicitly sized types, used size-explicit type names

--- types.h
+++ types.h
@@ -27,17 +27,21 @@
 #ifndef _BRICK_EMU_TYPES_H_
 #define  _BRICK_EMU_TYPES_H_
 
-typedef signed char int8;
-typedef unsigned char uint8;
-typedef signed short int16;
-typedef unsigned short uint16;
-typedef signed int int32;
-typedef unsigned int uint32;
+#include <stdint.h>
+
+typedef int8_t int8;
+typedef uint8_t uint8;
+typedef int16_t int16;
+typedef uint16_t uint16;
+typedef int32_t int32;
+typedef uint32_t uint32;
+typedef int64_t int64;
+typedef uint64_t uint64;
 
 /*
  * Cycle count main data type
  */
-typedef unsigned long long cycle_count_t;
+typedef uint64_t cycle_count_t;
 
 /*
  * Cycle count printf format
